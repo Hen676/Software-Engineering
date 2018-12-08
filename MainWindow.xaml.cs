fb92319588f72e7a46448e56596ffb2bbb97b756
Jozef Mystkowski <jozefmystkowski1@gmail.com>
2018-12-08 12:49:17 +0000

    Improved button usabilty + implemented short/long plot option

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,216 +24,289 @@ using TMDbLib.Objects.Search;
 
 namespace ACME_Movie_Client
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		ImdbEntity film = new ImdbEntity();
-		TmdbEntity film2 = new TmdbEntity();
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImdbEntity film = new ImdbEntity();
+        TmdbEntity film2 = new TmdbEntity();
 
-		bool searchByFilm = true;
-		bool searchByImdb = true;
+        bool searchByFilm = true;
+        bool searchByImdb = true;
+        bool searchShortPlot = true;
 
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void radioButton_Checked(object sender, RoutedEventArgs e)
-		{
-			searchByFilm = true;
-		}
+        void Button3(object sender, RoutedEventArgs e)
+        {
+            if (button3.Content.ToString() == "Search by: Title")
+            {
+                button3.Content = "Search by: ID";
+                searchByFilm = false;
+            }
+            else
+            {
+                button3.Content = "Search by: Title";
+                searchByFilm = true;
+            }
+        }
 
-		private void radioButton1_Checked(object sender, RoutedEventArgs e)
-		{
-			searchByFilm = false;
-		}
+        void Button4(object sender, RoutedEventArgs e)
+        {
+            if (button4.Content.ToString() == "Database: IMDB")
+            {
+                button4.Content = "Database: TMDB";
+                searchByImdb = false;
+            }
+            else
+            {
+                button4.Content = "Database: IMDB";
+                searchByImdb = true;
+            }
+        }
 
-		private void radioButton2_Checked(object sender, RoutedEventArgs e)
-		{
-			searchByImdb = true;
-		}
+        void Button5(object sender, RoutedEventArgs e)
+        {
+            if (button5.Content.ToString() == "Short plot")
+            {
+                button5.Content = "Long plot";
+                searchShortPlot = false;
 
-		private void radioButton3_Checked(object sender, RoutedEventArgs e)
-		{
-			searchByImdb = false;
-		}
+            }
+            else
+            {
+                button5.Content = "Short plot";
+                searchShortPlot = true;
+            }
+        }
 
-		private void Button1(object sender, RoutedEventArgs e)
-		{
-			List.Items.Add(textBlock1.Text);
+        //private void radioButton_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    searchByFilm = true;
+        //}
 
-		}
+        //private void radioButton1_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    searchByFilm = false;
+        //}
 
-		private void Button2(object sender, RoutedEventArgs e)
-		{
-			try
-			{
-				//string index = list.SelectedItem.ToString();
-				List.Items.Remove(List.SelectedItems[0]);
-			}
-			catch
-			{
-				MessageBox.Show("No item selected for deletion");
-			}
-		}
+        //private void radioButton2_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    searchByImdb = true;
+        //}
 
-		void PrintText(object sender, SelectionChangedEventArgs args)
-		{
-			string curItem = List.SelectedItem.ToString();
-		}
+        //private void radioButton3_Checked(object sender, RoutedEventArgs e)
+        //{
+        //    searchByImdb = false;
+        //}
 
-		//private void button_Click(object sender, RoutedEventArgs e)
-		//{
-		//    if (radioButton.IsChecked == true)
-		//    {
-		//        MessageBox.Show("Hello.");
-		//    }
-		//    else if (radioButton1.IsChecked == true)
-		//    {
-		//        MessageBox.Show("Goodbye.");
-		//    }
-		//}
+        private void Button1(object sender, RoutedEventArgs e)
+        {
+            string newString = textBlock1.Text;
+            try
+            {
+                List.Items.Add(newString.Remove(0, 7));
+            }
+            catch
+            {
+                MessageBox.Show("No film to add");
+            }
 
-		private void OnKeyDownHandler(object sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Return)
-			{
-				if (searchByImdb == true)
-				{
-					if (searchByFilm == true)
-					{
-						film = film.SearchTitle(textBox1.Text);
-					}
-					else
-					{
-						film = film.SearchID(textBox1.Text);
-					}
-					textBlock1.Text = "Title: " + film.Title;
-					textBlock2.Text = "Year: " + film.Year;
-					textBlock3.Text = "Rated: " + film.Rated;
-					textBlock3.Text = "Released: " + film.Released;
-					textBlock4.Text = "Runtime: " + film.Runtime;
-					textBlock5.Text = "Genre: " + film.Genre;
-					textBlock6.Text = "Director: " + film.Director;
-					textBlock7.Text = "Writer: " + film.Writer;
-					textBlock8.Text = "Actors: " + film.Actors;
-					textBlock9.Text = "Plot: " + film.Plot;
-					textBlock10.Text = "Language: " + film.Language;
-					textBlock11.Text = "Country: " + film.Country;
-					textBlock12.Text = "Awards: " + film.Awards;
-					textBlock13.Text = "Poster: " + film.Poster;
-					textBlock14.Text = "Metascore: " + film.Metascore;
-					textBlock15.Text = "imdb rating: " + film.imdbRating;
-					textBlock16.Text = "imdb votes: " + film.imdbVotes;
-					textBlock17.Text = "imdb ID: " + film.imdbID;
-					textBlock18.Text = "Type: " + film.Type;
-				}
-				else
-				{
-					if (searchByFilm == true)
-					{
-						//Add search by film functionality
-					}
-					else
-					{
-						Movie desiredFilm = film2.SearchID(textBox1.Text);
-						textBlock1.Text = "Title: " + desiredFilm.Title;
-						textBlock2.Text = "Release date: " + desiredFilm.ReleaseDate.ToString();
-						textBlock3.Text = "Popularity: " + desiredFilm.Popularity.ToString();
-						textBlock4.Text = "Runtime: " + desiredFilm.Runtime.ToString();
-						textBlock5.Text = "Budget: " + desiredFilm.Budget.ToString();
-						textBlock6.Text = "Poster link: " + desiredFilm.PosterPath;
-						textBlock7.Text = "Revenue: " + desiredFilm.Revenue.ToString();
-						textBlock8.Text = "Overview: " + desiredFilm.Overview;
-						textBlock9.Text = "Original language: " + desiredFilm.OriginalLanguage;
-						textBlock10.Text = "Keywords: " + desiredFilm.Keywords;
-						textBlock11.Text = "Adult: " + desiredFilm.Adult.ToString();
-						textBlock12.Text = "Vote average: " + desiredFilm.VoteAverage.ToString();
-						textBlock13.Text = "Vote count: " + desiredFilm.VoteCount.ToString();
-						textBlock14.Text = "";
-						textBlock15.Text = "";
-						textBlock16.Text = "";
-						textBlock17.Text = "";
-						textBlock18.Text = "";
-					}
-				}
+        }
 
-			}
-		}
-	}
-	public class ImdbEntity
-	{
-		HttpClient httpClient = new HttpClient();
+        private void Button2(object sender, RoutedEventArgs e)
+        {
 
-		public string filmNameModified;
+                //string index = list.SelectedItem.ToString();
+                List.Items.Remove(List.SelectedItems[0]);
+            
+        }
 
-		public string Title;
-		public string Year;
-		public string Rated;
-		public string Released;
-		public string Runtime;
-		public string Genre;
-		public string Director;
-		public string Writer;
-		public string Actors;
-		public string Plot;
-		public string Language;
-		public string Country;
-		public string Awards;
-		public string Poster;
-		public string Metascore;
-		public string imdbRating;
-		public string imdbVotes;
-		public string imdbID;
-		public string Type;
+        void PrintText(object sender, SelectionChangedEventArgs args)
+        {
+            try
+            {
+                string curItem = List.SelectedItem.ToString();
+                MessageBox.Show(curItem);
+            }
+            catch
+            {
 
-		public ImdbEntity SearchTitle(string searchText)
-		{
-			filmNameModified = searchText.Replace(" ", "+");
+            }
+        }
 
-			string response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + filmNameModified + "&apikey=8727e147")).Result;
-			ImdbEntity omdb = new JavaScriptSerializer().Deserialize<ImdbEntity>(response);
+        //private void button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (radioButton.IsChecked == true)
+        //    {
+        //        MessageBox.Show("Hello.");
+        //    }
+        //    else if (radioButton1.IsChecked == true)
+        //    {
+        //        MessageBox.Show("Goodbye.");
+        //    }
+        //}
 
-			if (omdb.Title == null)
-			{
-				omdb.Title = "No such film exists";
-			}
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (searchByImdb == true)
+                {
+                    if (searchByFilm == true)
+                    {
+                        try
+                        {
+                            film = film.SearchTitle(textBox1.Text, searchShortPlot);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Invalid film name");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        film = film.SearchID(textBox1.Text, searchShortPlot);
+                    }
+                    textBlock1.Text = "Title: " + film.Title;
+                    textBlock2.Text = "Year: " + film.Year;
+                    textBlock3.Text = "Rated: " + film.Rated;
+                    textBlock3.Text = "Released: " + film.Released;
+                    textBlock4.Text = "Runtime: " + film.Runtime;
+                    textBlock5.Text = "Genre: " + film.Genre;
+                    textBlock6.Text = "Director: " + film.Director;
+                    textBlock7.Text = "Writer: " + film.Writer;
+                    textBlock8.Text = "Actors: " + film.Actors;
+                    textBlock9.Text = "Plot: " + film.Plot;
+                    textBlock10.Text = "Language: " + film.Language;
+                    textBlock11.Text = "Country: " + film.Country;
+                    textBlock12.Text = "Awards: " + film.Awards;
+                    textBlock13.Text = "Poster: " + film.Poster;
+                    textBlock14.Text = "Metascore: " + film.Metascore;
+                    textBlock15.Text = "imdb rating: " + film.imdbRating;
+                    textBlock16.Text = "imdb votes: " + film.imdbVotes;
+                    textBlock17.Text = "imdb ID: " + film.imdbID;
+                    textBlock18.Text = "Type: " + film.Type;
+                }
+                else
+                {
+                    if (searchByFilm == true)
+                    {
+                        //Add search by film functionality
+                    }
+                    else
+                    {
+                        Movie desiredFilm = film2.SearchID(textBox1.Text);
+                        textBlock1.Text = "Title: " + desiredFilm.Title;
+                        textBlock2.Text = "Release date: " + desiredFilm.ReleaseDate.ToString();
+                        textBlock3.Text = "Popularity: " + desiredFilm.Popularity.ToString();
+                        textBlock4.Text = "Runtime: " + desiredFilm.Runtime.ToString();
+                        textBlock5.Text = "Budget: " + desiredFilm.Budget.ToString();
+                        textBlock6.Text = "Poster link: " + desiredFilm.PosterPath;
+                        textBlock7.Text = "Revenue: " + desiredFilm.Revenue.ToString();
+                        textBlock8.Text = "Overview: " + desiredFilm.Overview;
+                        textBlock9.Text = "Original language: " + desiredFilm.OriginalLanguage;
+                        textBlock10.Text = "Keywords: " + desiredFilm.Keywords;
+                        textBlock11.Text = "Adult: " + desiredFilm.Adult.ToString();
+                        textBlock12.Text = "Vote average: " + desiredFilm.VoteAverage.ToString();
+                        textBlock13.Text = "Vote count: " + desiredFilm.VoteCount.ToString();
+                        textBlock14.Text = "";
+                        textBlock15.Text = "";
+                        textBlock16.Text = "";
+                        textBlock17.Text = "";
+                        textBlock18.Text = "";
+                    }
+                }
 
-			return omdb;
-		}
+            }
+        }
+    }
+    public class ImdbEntity
+    {
+        HttpClient httpClient = new HttpClient();
 
-		public ImdbEntity SearchID(string filmID)
-		{
-			string response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?i=" + filmID + "&apikey=8727e147")).Result;
-			ImdbEntity omdb = new JavaScriptSerializer().Deserialize<ImdbEntity>(response);
+        public string filmNameModified;
 
-			if (omdb.Title == null)
-			{
-				omdb.Title = "No such ID exists";
-			}
+        public string Title;
+        public string Year;
+        public string Rated;
+        public string Released;
+        public string Runtime;
+        public string Genre;
+        public string Director;
+        public string Writer;
+        public string Actors;
+        public string Plot;
+        public string Language;
+        public string Country;
+        public string Awards;
+        public string Poster;
+        public string Metascore;
+        public string imdbRating;
+        public string imdbVotes;
+        public string imdbID;
+        public string Type;
 
-			return omdb;
-		}
-	}
+        public ImdbEntity SearchTitle(string searchText, bool shortPlot)
+        {
+            filmNameModified = searchText.Replace(" ", "+");
+            string response;
 
-	class TmdbEntity
-	{
-		Movie movie;
-		public Movie SearchID(string searchText)
-		{
-			try
-			{
-				int id = Convert.ToInt32(searchText);
-				TMDbClient client = new TMDbClient("1206a7b3fff9fa9951f843442e161c6a");
-				movie = client.GetMovieAsync(id).Result;
-			}
-			catch
-			{
-				MessageBox.Show("ID contains no letters");
-			}
-			return movie;
-		}
-	}
+            if (shortPlot == true)
+            response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + filmNameModified + "&apikey=8727e147")).Result;
+            else
+            response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?t=" + filmNameModified + "&plot=full&apikey=8727e147")).Result;
+
+            ImdbEntity omdb = new JavaScriptSerializer().Deserialize<ImdbEntity>(response);
+
+            if (omdb.Title == null)
+            {
+                omdb.Title = "No such film exists";
+            }
+
+            return omdb;
+        }
+
+        public ImdbEntity SearchID(string filmID, bool shortPlot)
+        {
+            string response;
+
+            if (shortPlot == true)
+            response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?i=" + filmID + "&apikey=8727e147")).Result;
+            else
+            response = httpClient.GetStringAsync(new Uri("http://www.omdbapi.com/?i=" + filmID + "&plot=full&apikey=8727e147")).Result;
+            ImdbEntity omdb = new JavaScriptSerializer().Deserialize<ImdbEntity>(response);
+
+            if (omdb.Title == null)
+            {
+                omdb.Title = "No such ID exists";
+            }
+
+            return omdb;
+        }
+    }
+
+    class TmdbEntity
+    {
+        Movie movie;
+        public Movie SearchID(string searchText)
+        {
+            try
+            {
+                int id = Convert.ToInt32(searchText);
+                TMDbClient client = new TMDbClient("1206a7b3fff9fa9951f843442e161c6a");
+                movie = client.GetMovieAsync(id).Result;
+            }
+            catch
+            {
+                MessageBox.Show("ID contains no letters");
+            }
+            return movie;
+        }
+    }
 }
